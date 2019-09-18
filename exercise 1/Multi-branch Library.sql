8b07b475efad9562e00cad2617683516315fd8f6
lakshay <lakshay@vinsol.com>
2019-09-18 16:54:11 +0530

    commiting

---
+++ exercise 1/Multi-branch Library.sql
@@ -0,0 +1,66 @@
+USE schm;
+
+CREATE TABLE branches 
+( 
+bcode VARCHAR(4) NOT NULL PRIMARY KEY,
+librarian VARCHAR(45) NULL,
+address VARCHAR(45) NULL
+);
+
+INSERT INTO branches VALUE ('B1', 'John Smith', '2 Anglesea Rd'),
+('B2', 'Mary Jones', '34 Pearse St'),
+('B3', 'Francis Owens', 'Grange X');
+
+CREATE TABLE titles 
+(
+title VARCHAR(45) NOT NULL PRIMARY KEY,
+author VARCHAR(45) NULL,
+publisher VARCHAR(45) NULL
+);
+
+INSERT INTO titles VALUE ('Susannah', 'Ann Brown', 'Macmillan'),
+('How to Fish', 'Amy Fly', 'Stop Press'), 
+('A History of Dublin', 'David Little', 'Wiley'),
+('Computers', 'Blaise Pascal', 'Applewoods'),
+('the Wife', 'Ann Brown', 'Macmillan');
+
+CREATE TABLE holdings 
+(
+branch VARCHAR(4) NOT NULL,
+title VARCHAR(45) NULL,
+copies VARCHAR(45) NULL,
+FOREIGN KEY (branch) REFERENCES branches (bcode),
+FOREIGN KEY (title) REFERENCES titles (title)
+);
+
+INSERT INTO holdings VALUE ('B1', 'Susannah', 3),
+('B1', 'How to Fish', 2),
+('B1', 'A History of Dublin', 1),
+('B2', 'How to Fish', 4),
+('B2', 'Computers', 2),
+('B2', 'the Wife', 3),
+('B3', 'A History of Dublin', 1),
+('B3', 'Computers', 4),
+('B3', 'Susannah', 3),
+('B3', 'the Wife', 1);
+
+	-- the names of all library books published by Macmillan --
+SELECT title from titles
+WHERE publisher='Macmillan';
+
+	-- branches that hold any books by Ann Brown (using a nested subquery) --
+SELECT DISTINCT branch from holdings
+WHERE title IN
+(
+SELECT title FROM titles
+WHERE author='Ann Brown'
+);
+
+	-- branches that hold any books by Ann Brown (without using a nested subquery) --
+SELECT branch, t.title FROM holdings AS h JOIN titles AS t
+ON h.title=t.title 
+WHERE t.author='Ann Brown'
+ORDER BY branch;
+
+	-- the total number of books held at each branch --
+SELECT branch, SUM(copies) FROM holdings GROUP BY branch;
---
+++ exercise 1/Sandwich.sql
@@ -0,0 +1,62 @@
+USE schm;
+
+CREATE TABLE tastes 
+(
+  name VARCHAR(15) NOT NULL,
+  filling VARCHAR(15) NOT NULL,
+  PRIMARY KEY (name,filling)
+);
+INSERT INTO tastes(name, filling) VALUES ('Brown', 'Turkey'),
+('Brown', 'Beef'),
+('Brown', 'Ham'),
+('Jones', 'Cheese'),
+('Green', 'Beef'),
+('Green', 'Turkey'),
+('Green', 'Cheese');
+
+CREATE TABLE locations 
+(
+  lname VARCHAR(15) NOT NULL PRIMARY KEY,
+  phone INT NOT NULL ,
+  address VARCHAR(45) NOT NULL
+);
+INSERT INTO locations (lname, phone, address) VALUES ('Lincoln', '6834523', 'Lincoln Place'),
+('O\'Neill\'s', '6742134', 'Pearse St'),
+('Old Nag', '7678132', 'Dame St'),
+('Buttery', '7023421', 'College St');
+
+CREATE TABLE sandwiches 
+(
+  location VARCHAR(15) NOT NULL,
+  bread VARCHAR(15) NOT NULL,
+  filling VARCHAR(45) NOT NULL,
+  price INT NOT NULL,
+  FOREIGN KEY (location) REFERENCES locations(lname)
+);
+INSERT INTO sandwiches (location, bread, filling, price) VALUES ('Lincoln', 'Rye', 'Ham', '1.25'),
+('O\'Neill\'s', 'White', 'Cheese', 1.20),
+('O\'Neill\'s', 'Whole', 'Ham', 1.25),
+('Old Nag', 'Rye', 'Beef', 1.35),
+('Buttery', 'White', 'Cheese', 1.00),
+('O\'Neill\'s', 'White', 'Turkey', 1.35),
+('Buttery', 'White', 'Ham', 1.10),
+('Lincoln', 'Rye', 'Beef', 1.35),
+('Lincoln', 'White', 'Ham', 1.3),
+('Old Nag', 'Rye', 'Ham', 1.4);
+
+	-- places where Jones can eat (using a nested subquery) --
+SELECT DISTINCT location FROM sandwiches 
+WHERE filling IN 
+(
+SELECT filling FROM tastes AS T WHERE t.name='Jones'
+);
+
+	-- places where Jones can eat (without using a nested subquery) --
+SELECT location FROM sandwiches AS s, tastes AS t
+WHERE t.filling=s.filling AND t.name='jones';
+
+	-- for each location the number of people who can eat there --
+SELECT location, COUNT(distinct name) AS Total_no_of_People
+FROM sandwiches AS s JOIN tastes AS t
+WHERE s.filling=t.filling
+GROUP BY location;
